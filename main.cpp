469702a4aab15870b0540bf5e1155ea01e3acd91
Saúl García <166766878+SaulAngel18@users.noreply.github.com>
2026-02-16 20:51:06 -0600

    PRINCIPAL
    se hizo las modificaciones mas extensas

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Que viva la vida en el Carnaval de Sayula!" << endl;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    // Amarillo brillante
+    SetConsoleTextAttribute(hConsole, 14);
+    cout << "=============================================" << endl;
+
+    // Magenta
+    SetConsoleTextAttribute(hConsole, 13);
+    cout << "  QUE VIVA LA VIDA " << endl;
+
+    // Verde brillante
+    SetConsoleTextAttribute(hConsole, 10);
+    cout << "   EN EL CARNAVAL DE SAYULA!!! " << endl;
+
+    // Azul
+    SetConsoleTextAttribute(hConsole, 11);
+    cout << "   Musica, color y tradicion " << endl;
+
+    // Rojo brillante
+    SetConsoleTextAttribute(hConsole, 12);
+    cout << "=============================================" << endl;
+
+    // Regresar a color blanco normal
+    SetConsoleTextAttribute(hConsole, 7);
+
     return 0;
 }
